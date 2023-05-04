3eb23be72a8feb085b7af993a01d265339cccfdb
Miki <chisparroja@gmail.com>
2023-05-04 22:02:10 +0200

    [CLEANUP] Removed unused Prisma service from JwtStrategy class

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -2,11 +2,10 @@ import { ExtractJwt, Strategy } from "passport-jwt"
 import { PassportStrategy } from "@nestjs/passport";
 import { NotFoundException, Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config"
-import { PrismaService } from "../../prisma/prisma.service";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-	constructor(config: ConfigService, private prisma: PrismaService) {
+	constructor(config: ConfigService) {
 		super({
 			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
 			ignoreExpiration: true,
