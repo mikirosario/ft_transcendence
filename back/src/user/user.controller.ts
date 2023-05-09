4aeeef504978f5ea158e7d84d20acd7a6c0648e6
karisti- <kepaaristi@gmail.com>
2023-05-09 21:23:14 +0200

    Feature: user profile nick and avatar endpoints

--- back/src/user/user.controller.ts
+++ back/src/user/user.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get, UseGuards, Req, Patch, Body, NotFoundException, Delete } from "@nestjs/common";
+import { Controller, Get, UseGuards, Req, Patch, Body, NotFoundException, Delete, Post, UseInterceptors, UploadedFile, Put } from "@nestjs/common";
 import { ApiBody, ApiBearerAuth } from "@nestjs/swagger"
 import { JwtGuard } from "../auth/guard";
 import { GetJwt } from "../auth/decorator";
-import { EditUserDto } from "./dto";
+import { EditUserDto, UserProfileDto } from "./dto";
 import { UserService } from "./user.service";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { diskStorage } from "multer";
+import { v4 as uuidv4 } from 'uuid';
+
 
 @UseGuards(JwtGuard)
 @Controller('users')
@@ -25,4 +29,41 @@ export class UserController {
 	async deleteUser(@GetJwt('sub') userId: number) {
 		return this.userService.deleteUser(userId);
 	}
+
+	/*
+	 * Set / update user profile data
+	*/
+	@Put('profile')
+	@ApiBody({ type: UserProfileDto })
+	async updateProfileData(@GetJwt('sub') userId: number, @Body() dto: UserProfileDto) {
+		return this.userService.updateProfileData(userId, dto);
+	}
+
+	/*
+	 * Set / update user profile picture
+	*/
+	@Post('profile')
+	@UseInterceptors(FileInterceptor('file', {
+		storage: diskStorage({
+			destination: './files/avatars',
+			filename: (req, file, cb) => {
+				const filename: string = uuidv4();
+				const path = require('path');
+				const extension: string = path.parse(file.originalname).ext;
+				cb(null, filename + extension)
+			}
+		})
+	}))
+	async uploadProfilePicture(@GetJwt('sub') userId: number, @UploadedFile() file?: Express.Multer.File) {
+		return this.userService.uploadProfilePicture(userId, file);
+	}
+
+	/*
+	 * Remove user profile picture
+	*/
+	@Delete('profile')
+	async deleteProfilePicture(@GetJwt('sub') userId: number) {
+		return this.userService.deleteProfilePicture(userId);
+	}
+	
 }
--- back/src/user/user.service.ts
+++ back/src/user/user.service.ts
@@ -2,11 +2,14 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 import { PrismaService } from '../prisma/prisma.service';
 import { ThrowHttpException } from '../utils/error-handler';
-import { EditUserDto } from './dto';
+import { EditUserDto, UserProfileDto } from "./dto";
+import { ConfigService } from "@nestjs/config";
+import * as fs from 'fs';
+
 
 @Injectable()
 export class UserService {
-	constructor(private prisma: PrismaService) { }
+	constructor(private prisma: PrismaService, private config: ConfigService) { }
 	async getMe(userId: number) {
 		const user = await this.prisma.user.findUnique({
 			where: {
@@ -19,6 +22,7 @@ export class UserService {
 		delete user.hash;
 		return user;
 	}
+
 	async editUser(userId: number, dto: EditUserDto) {
 		try {
 			const user = await this.prisma.user.update({
@@ -54,6 +58,132 @@ export class UserService {
 			if (error instanceof PrismaClientKnownRequestError) {
 				ThrowHttpException(error, 'User not found');
 			}
+		}
+	}
+
+	/*
+	 * Set / update user profile data
+	*/
+	async updateProfileData(userId: number, dto: UserProfileDto) {
+
+		try {
+			const user = await this.prisma.user.update({
+				where: {
+					id: userId
+				},
+				data: {
+					...dto
+				},
+			});
+
+			delete user.hash;
+			return user;
+		}
+		catch (error) {
+			if (error instanceof PrismaClientKnownRequestError) {
+				// https://www.prisma.io/docs/reference/api-reference/error-reference
+				// P2025 Record not found
+				ThrowHttpException(error, 'User not found');
+			}
+		}
+	}
+
+	/*
+	 * Set / update user profile picture
+	*/
+	async uploadProfilePicture(userId: number, file: Express.Multer.File) {
+
+		const user = await this.prisma.user.findUnique({
+			where: {
+				id: userId,
+			}
+		});
+
+		if (user === null) {
+			if (file)
+			{
+				this.removeAvatar(file.path)
+			}
+			
+			ThrowHttpException(new NotFoundException, 'User not found');
+		}
+
+		const prevAvatar = user.avatarUri;
+
+		try {
+			const user = await this.prisma.user.update({
+				where: {
+					id: userId
+				},
+				data: {
+					avatarUri: file.path
+				},
+			});
+
+			this.removeAvatar(prevAvatar);
+
+			delete user.hash;
+			return user;
+		}
+		catch (error) {
+			if (error instanceof PrismaClientKnownRequestError) {
+				// https://www.prisma.io/docs/reference/api-reference/error-reference
+				// P2025 Record not found
+				ThrowHttpException(error, 'User not found');
+			}
+		}
+	}
+
+	/*
+	 * Remove user profile picture
+	*/
+	async deleteProfilePicture(userId: number) {
+
+		const user = await this.prisma.user.findUnique({
+			where: {
+				id: userId,
+			}
+		});
+
+		if (user === null) {
+			ThrowHttpException(new NotFoundException, 'User not found');
+		}
+
+		const avatar = user.avatarUri;
+
+		try {
+			const user = await this.prisma.user.update({
+				where: {
+					id: userId
+				},
+				data: {
+					avatarUri: this.config.get('DEFAULT_AVATAR_URI')
+				},
+			});
+
+			this.removeAvatar(avatar);
+
+			delete user.hash;
+			return user;
+		}
+		catch (error) {
+			if (error instanceof PrismaClientKnownRequestError) {
+				// https://www.prisma.io/docs/reference/api-reference/error-reference
+				// P2025 Record not found
+				ThrowHttpException(error, 'User not found');
+			}
+		}
+	}
+
+	/*
+	 * Remove avatar file
+	*/
+	async removeAvatar(filePath: string) {
+		if (filePath && filePath !== this.config.get('DEFAULT_AVATAR_URI'))
+		{
+			fs.unlink(filePath, (err) => {
+				if (err) throw err;
+			})
 		}
 	}
 }
