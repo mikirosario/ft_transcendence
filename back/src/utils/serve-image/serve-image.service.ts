7aeceeb04b7182edd2c028e88708413f9286eeff
karisti- <kepaaristi@gmail.com>
2023-05-09 23:02:02 +0200

    serveImage image not found error handled

--- back/src/utils/serve-image/serve-image.service.ts
+++ back/src/utils/serve-image/serve-image.service.ts
@@ -3,14 +3,21 @@ import { PrismaService } from '../../prisma/prisma.service';
 import { ThrowHttpException } from '../../utils/error-handler';
 import { Response } from "express";
 import { join } from 'path';
+import * as fs from 'fs';
 
 
 @Injectable()
 export class ServeImageService {
 	constructor(private prisma: PrismaService) { }
 
-	async serveImage(userId: number, filename: string, res: Response) {
-		return res.sendFile(join(__dirname, '../../../uploads/avatars/', filename));
+	async serveImage(userId: number, fileName: string, res: Response) {
+		const filePath = join(__dirname, '../../../uploads/avatars/', fileName);
+
+		if (!fs.existsSync(filePath)) {
+			return res.status(404).send('El archivo no existe');
+		}
+		
+		return res.sendFile(filePath);
 	}
 
 }
