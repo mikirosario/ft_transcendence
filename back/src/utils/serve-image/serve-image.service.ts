379e52c8f4e4e7a31f584fe4f738ab5cabc5c98d
karisti- <kepaaristi@gmail.com>
2023-05-09 23:16:48 +0200

    default avatar returned if avatar is not found

--- back/src/utils/serve-image/serve-image.service.ts
+++ back/src/utils/serve-image/serve-image.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
-import { ThrowHttpException } from '../../utils/error-handler';
+import { ConfigService } from "@nestjs/config";
 import { Response } from "express";
 import { join } from 'path';
 import * as fs from 'fs';
@@ -8,16 +8,20 @@ import * as fs from 'fs';
 
 @Injectable()
 export class ServeImageService {
-	constructor(private prisma: PrismaService) { }
+	constructor(private prisma: PrismaService, private config: ConfigService) { }
 
 	async serveImage(userId: number, fileName: string, res: Response) {
 		const filePath = join(__dirname, '../../../uploads/avatars/', fileName);
+		if (fs.existsSync(filePath)) {
+			return res.sendFile(filePath);
+		}
 
-		if (!fs.existsSync(filePath)) {
-			return res.status(404).send('El archivo no existe');
+		const defaultPath = join(__dirname, '../../../', this.config.get('DEFAULT_AVATAR_URI'));
+		if (fs.existsSync(defaultPath)) {
+			return res.sendFile(defaultPath);
 		}
 		
-		return res.sendFile(filePath);
+		return res.status(404).send('El archivo no existe');
 	}
 
 }
