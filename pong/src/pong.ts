41426d3acc702e5619fe14a93ad4476da08cefc0
Miki <chisparroja@gmail.com>
2023-05-10 19:55:29 +0200

    [PONG:BASE] Refactored game initialization.
    New Pong class is now instantiated to handle gameplay. RequestAnimationFrame is now used for browser animation instead of SetInterval. Right paddle added. Logic added to Rectangle to get upper left corner coordinates. Rectangle and Circle classes now draw themselves based on their transform position coordinates.

--- pong/src/pong.ts
+++ pong/src/pong.ts
@@ -1,6 +1,6 @@
 import { getGameCanvas, getGameRenderingContext, initGameCanvas, fetchColorConstants } from "./init.js";
 import { showError } from "./utils.js";
-import { AspectRatio, IDrawable, Rectangle, Circle, Transform } from "./types.js";
+import { AspectRatio, IDrawable, Rectangle, Circle, Transform, Pong } from "./types.js";
 
 let canvasBackgroundColor: string;
 
@@ -13,33 +13,14 @@ async function main() {
         const aspectRatio: AspectRatio = { width: 858, height: 525 };
         const colorConstants = await fetchColorConstants();
         canvasBackgroundColor = colorConstants.canvasBackgroundColor;
-        const leftPaddle = new Rectangle(new Transform({ x: 0, y: 0 }, 1), "black", 10, 100);
-        const circle = new Circle(new Transform({ x: 200, y: 200 }, 0), "white", 10);
-        const drawables: IDrawable[] = [ leftPaddle, circle ];
         initGameCanvas(canvas, ctx, aspectRatio, canvasBackgroundColor);
-        setInterval(() => { renderFrame(drawables, ctx, canvas, canvasBackgroundColor); }, 1000);
+        const pong = new Pong(canvas, ctx, aspectRatio, canvasBackgroundColor);
     }
     catch (err: any)
     {
         showError(err.message)
     }
 }
-
-function clearScreen(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement, background: string)
-{
-    ctx.fillStyle = background;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-}
-
-function renderFrame(drawables: IDrawable[], ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement, background: string)
-{
-    clearScreen(ctx, canvas, background);
-    drawables.forEach((drawable) => {
-        drawable.draw(ctx);
-    })
-}
-
-
 
 window.onload = function() {
     main();
--- pong/src/types.ts
+++ pong/src/types.ts
@@ -1,3 +1,6 @@
+import { ColorConstants } from "./colors.constants";
+import { initGameCanvas } from "./init";
+
 export type AspectRatio = { width: number, height: number };
 
 export type Position = { x: number, y: number };
@@ -34,10 +37,19 @@ export class Rectangle implements IDrawable
         this.height = height;
     }
 
+    private getUpperLeftCorner(): Position
+    {
+        let halfWidth = Math.round(this.width * 0.5);
+        let halfHeight = Math.round(this.height * 0.5);
+
+        return { x: this.transform.position.x - halfWidth, y: this.transform.position.y - halfHeight};
+    }
+
     public draw(ctx: CanvasRenderingContext2D): void
     {
+        let upperLeftCornerPosition = this.getUpperLeftCorner();
         ctx.fillStyle = "black";
-        ctx.fillRect(100, 200, this.width, this.height);
+        ctx.fillRect(upperLeftCornerPosition.x, upperLeftCornerPosition.y, this.width, this.height);
     }
 }
 
@@ -58,7 +70,7 @@ export class Circle implements IDrawable
     {
         ctx.fillStyle = "white";
         ctx.beginPath();
-        ctx.arc(300, 350, this.radius, 0, Math.PI * 2, false);
+        ctx.arc(this.transform.position.x, this.transform.position.y, this.radius, 0, Math.PI * 2, false);
         ctx.closePath();
         ctx.fill();
     }
@@ -84,4 +96,50 @@ export class Text
         ctx.font = this.font;
         ctx.fillText(this.text, this.transform.position.x, this.transform.position.y);
     }
+}
+
+export class Pong
+{
+    private canvas: HTMLCanvasElement;
+    private ctx: CanvasRenderingContext2D;
+    private aspectRatio: AspectRatio;
+    private backgroundColor: string;
+    private leftPaddle: Rectangle;
+    private rightPaddle: Rectangle;
+    private ball: Circle;
+    private drawables: IDrawable[];
+
+    constructor(
+        canvas: HTMLCanvasElement,
+        context: CanvasRenderingContext2D,
+        aspectRatio: AspectRatio,
+        backgroundColor: string)
+    {
+        this.canvas = canvas;
+        this.ctx = context;
+        this.aspectRatio = aspectRatio;
+        this.backgroundColor = backgroundColor;
+        this.leftPaddle = new Rectangle(new Transform({ x: 100, y: Math.round(this.canvas.height * 0.5) }, 1), "black", 10, 100);
+        this.rightPaddle = new Rectangle(new Transform({ x: this.canvas.width - 100, y: Math.round(this.canvas.height * 0.5) }, 1), "black", 10, 100);
+        this.ball = new Circle(new Transform({ x: Math.round(this.canvas.width * 0.5), y: Math.round(this.canvas.height * 0.5) }, 1), "white", 10);
+        this.drawables = [ this.leftPaddle, this.rightPaddle, this.ball ];
+        this.renderFrame = this.renderFrame.bind(this);
+        requestAnimationFrame(this.renderFrame);
+    }
+
+    clearScreen()
+    {
+        this.ctx.fillStyle = this.backgroundColor;
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
+    renderFrame()
+    {
+        this.clearScreen();
+        this.drawables.forEach((drawable) => {
+            drawable.draw(this.ctx);
+        requestAnimationFrame(this.renderFrame)
+    })
+}
+
 }
