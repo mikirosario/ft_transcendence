5b785474e91f6fe43e5f4600817852c02c32628f
karisti- <kepaaristi@gmail.com>
2023-05-13 11:16:54 +0200

    enable cors to allow requests only from our front

--- back/src/main.ts
+++ back/src/main.ts
@@ -6,6 +6,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.useGlobalPipes(new ValidationPipe({ whitelist: true, }));
+	app.enableCors({
+		origin: 'http://localhost:3001'
+	  });
 
 	const swaggerConfig = new DocumentBuilder()
 		.setTitle('PostgreSQL API')
