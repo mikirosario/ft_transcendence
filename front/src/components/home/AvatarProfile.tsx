aa3bb46e91d8eb399c5f33fded00fb1f92280383
gpernas- <gonper88@gmail.com>
2023-05-14 23:59:01 +0200

    UserProfileLogged Component

--- front/src/components/home/AvatarProfile.tsx
+++ front/src/components/home/AvatarProfile.tsx
@@ -8,22 +8,33 @@ interface Args {
 }
 
 const InGameUserProfile: React.FC<Args> = (args) => {
+  const avatarStyle = {
+    width: 75,
+    height: 75,
+    borderRadius: '50%',
+    marginRight: 15
+  };
+
+  const nameStyle = {
+    fontSize: 16
+  };
+
+  const rankStyle = {
+    fontSize: 13,
+    fontWeight: 'bold'
+  };
+
   return (
-    <div>
+    <div style={{ display: 'flex', alignItems: 'center' }}>
       <img
         className="avatar"
         src={args.image}
         alt="avatar"
-        style={{
-          width: 75,
-          height: 75,
-          borderRadius: '50%',
-          marginRight: 15
-        }}
+        style={avatarStyle}
       />
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
-        <div style={{ fontSize: 16 }}>{args.name} <span style={{ fontSize: 13, fontWeight: 'bold' }}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;#{args.rank}</span></div>
-        {/* <div style={{ fontSize: 13, marginTop: 2}}>Nivel: {props.level}</div> */}
+        <div style={nameStyle}>{args.name}</div>
+        <div style={rankStyle}>#{args.rank}</div>
       </div>
     </div>
   );
