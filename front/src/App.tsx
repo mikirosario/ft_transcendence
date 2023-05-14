69f9817d2907ff8bbefaf4cbc6e07e045829cdc0
gpernas- <gonper88@gmail.com>
2023-05-15 00:08:20 +0200

    Socket Connection

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,8 +3,21 @@ import { Route, Link, Router } from "react-router-dom";
 import logo from './assets/images/logo.svg';
 import Home from './pages/Home';
 import Options from './pages/Options';
-
 import './assets/css/App.css';
+import { io, Socket } from 'socket.io-client';
+
+
+const socketOptions = {
+  transportOptions: {
+      polling: {
+          extraHeaders: {
+              Authorization: localStorage.getItem("token"),
+          }
+      }
+  }
+};
+
+const socket: Socket = io('http://localhost:8081/', socketOptions);
 
 const App: React.FC = () => {
   return (
