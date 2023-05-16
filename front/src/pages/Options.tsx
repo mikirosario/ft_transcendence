2fb5b4ab0fcabb16c36ee4af34c50976e4593f1b
gpernas- <gonper88@gmail.com>
2023-05-17 01:20:12 +0200

    PreRegister + Register +Settings + home pages

--- front/src/pages/Options.tsx
+++ front/src/pages/Options.tsx
@@ -1,16 +1,53 @@
 import React from "react";
-import OptionsButton from "../components/settings/Options";
+import OptionsButton from "../components/settings/SettingsButtons";
+import GoToHomepage from "../components/home/Home";
 
 function Options() {
+
+    const NicknamePositionStyle: React.CSSProperties = {
+        height: '320px',
+        width: '100%',
+        top: '320px',
+        backgroundColor: '#01624',
+    };
+
+    const NicknameTapeStyle: React.CSSProperties = {
+        height: '100%',
+        width: '100%',
+        top: '260px',
+        backgroundColor: '#1c2c49',
+        position: 'relative',
+        display: 'flex',
+        justifyContent: 'center',
+    };
+
+    const NicknameInputStyle: React.CSSProperties = {
+        height: '74px',
+        left: '50%',
+        position: 'absolute',
+        top: '220px',
+        width: '80%',
+        maxWidth: '300px',
+        transform: 'translateX(-50%)',
+    };
+
+
+
     return (
-        <h1>
-            this is the option page
-            <div>
-                <section className="OptionsMenu">
-                    <OptionsButton></OptionsButton>
-                </section>
+        <div className="Homepage">
+            <section>
+                <GoToHomepage></GoToHomepage>
+            </section>
+            <div className="NicknamePositionWrapper" style={NicknamePositionStyle}>
+                <div className="NicknameTapeWrapper" style={NicknameTapeStyle}>
+                    <div className="NicknameInputWrapper" style={NicknameInputStyle}>
+                        <section className="OptionsMenu">
+                            <OptionsButton></OptionsButton>
+                        </section>
+                    </div>
+                </div>
             </div>
-        </h1>
+        </div>
     )
 }
 
--- front/src/pages/Register.tsx
+++ front/src/pages/Register.tsx
@@ -1,34 +1,146 @@
 import React from "react";
 import { useNavigate } from 'react-router-dom';
-import PlayButton from "../components/home/PlayButton";
-import SettingsButton from "../components/home/SettingsButton";
-import SwitchChatLadder from "../components/chat-ladder/ChatLadderSwitch";
+import { BiSend } from 'react-icons/bi';
+import defaultIMG from '../assets/images/default.jpg'
 
-function Home() {
+function Register() {
     const navigate = useNavigate();
 
-    const AuthLoginStyle: React.CSSProperties = {
-        position: 'absolute',
-        top: '80%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-      };
+    const ConfirmUserLink = () => {
+        // Makes the fetch action to check if the user is valid 
 
-    const AuthLoginLink = () => {
         // window.location.href = 'xxxx'; // Auth URL
         navigate('/homepage');
     };
 
+    // const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    //     if (event.target.files && event.target.files.length > 0) {
+    //       const selectedImage = event.target.files[0];
+    //       setImage(selectedImage);
+    //     }
+    //   };
+
+    const NicknamePositionStyle: React.CSSProperties = {
+        height: '320px',
+        width: '100%',
+        top: '320px',
+        backgroundColor: '#01624',
+        display: 'flex',
+        justifyContent: 'center',
+    };
+
+    const NicknameTapeStyle: React.CSSProperties = {
+        height: '100%',
+        width: '100%',
+        top: '260px',
+        backgroundColor: '#1c2c49',
+        position: 'relative',
+    };
+
+    const TitleStyle: React.CSSProperties = {
+        color: '#ffffff',
+        fontFamily: "'Press Start 2P'",
+        fontSize: '40px',
+        fontWeight: 400,
+        height: '40px',
+        lineHeight: 'normal',
+        position: 'absolute',
+        textAlign: 'center',
+        top: '50px',
+        whiteSpace: 'nowrap',
+        width: '100%',
+        left: '50%',
+        transform: 'translateX(-50%)',
+    };
+
+    const SubtitleStyle: React.CSSProperties = {
+        color: '#ffffff',
+        fontFamily: "'Press Start 2P'",
+        fontSize: '20px',
+        fontWeight: 400,
+        height: '48px',
+        lineHeight: 'normal',
+        position: 'absolute',
+        textAlign: 'center',
+        top: '120px',
+        width: '100%',
+        left: '50%',
+        transform: 'translateX(-50%)',
+    };
+
+    const NicknameInputStyle: React.CSSProperties = {
+        height: '74px',
+        left: '50%',
+        position: 'absolute',
+        top: '220px',
+        width: '80%',
+        maxWidth: '593px',
+        transform: 'translateX(-50%)',
+    };
+
+    const ImageStyle: React.CSSProperties = {
+        height: '75px',
+        left: '-20px',
+        position: 'absolute',
+        top: '-22px',
+        width: '75px',
+        borderRadius: '50%',
+    };
+
+    const InputTextStyle: React.CSSProperties = {
+        color: 'white',
+        fontFamily: "'Press Start 2P'",
+        height: '25px',
+        left: '75px',
+        position: 'absolute',
+        top: '20px',
+        width: '475px',
+        border: 'none',
+        borderBottom: '2px solid gray',
+        background: 'transparent',
+    };
+
+
+    const SendButtonStyle: React.CSSProperties = {
+        color: 'white',
+        fontSize: '25px',
+        cursor: 'pointer',
+        position: 'absolute',
+        top: '21px',
+        right: '10px',
+    };
 
     return (
-        <div>
-            <h1> Register page with 42 Auth </h1>
-            <section className='AuthLoginButton' style={AuthLoginStyle} onClick={AuthLoginLink}>
-                <PlayButton name="Login"/>
-            </section>
-
+        <div className="NicknamePositionWrapper" style={NicknamePositionStyle}>
+            <div className="NicknameTapeWrapper" style={NicknameTapeStyle}>
+                <div className="NicknameTitle" style={TitleStyle}>
+                    CHOOSE A NICKNAME
+                </div>
+                <div className="NicknameSubtitle" style={SubtitleStyle}>
+                    Nick rules
+                </div>
+                <div className="NicknameInputWrapper" style={NicknameInputStyle}>
+                    <img
+                        className="NicknameImage"
+                        alt="default"
+                        src={defaultIMG}
+                        style={ImageStyle}
+                    // Onclick Tag to set an image
+                    />
+                    <input
+                        type="text"
+                        placeholder="Enter your nickname"
+                        style={InputTextStyle}
+                    />
+                    <BiSend
+                        className="SendButton"
+                        style={SendButtonStyle}
+                        onClick={ConfirmUserLink}
+                    />
+                </div>
+            </div>
         </div>
     );
 }
 
-export default Home;
+export default Register;
