dfd4dc0fd0e567f61568b3481565649f0126a938
gpernas- <gonper88@gmail.com>
2023-05-17 01:18:56 +0200

    File renames + update components

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Link, Router } from "react-router-dom";
 import logo from './assets/images/logo.svg';
-import Home from './pages/Home';
+import Home from './pages/Homepage';
 import Options from './pages/Options';
 import './assets/css/App.css';
 import { io, Socket } from 'socket.io-client';
