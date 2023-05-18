dd3886f3e3efbeb7fba1fa8c60ba536424a9cf7c
karisti- <kepaaristi@gmail.com>
2023-05-18 23:54:46 +0200

    friend dto

--- back/src/friend/dto/friend.dto.ts
+++ back/src/friend/dto/friend.dto.ts
@@ -2,7 +2,7 @@ import { IsNotEmpty, MinLength, MaxLength } from "class-validator";
 import { ApiProperty } from "@nestjs/swagger"
 import { IsNick } from '../../utils/decorators/is-nick.decorator';
 
-export class AddFriendDto {
+export class FriendDto {
 	@ApiProperty()
 	@IsNotEmpty()
 	@MinLength(3)
