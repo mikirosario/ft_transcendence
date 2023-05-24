084f290788089f5adb2a10cc5f7b94a7ddd5618d
karisti- <kepaaristi@gmail.com>
2023-05-24 20:02:38 +0200

    chat empty template

---
+++ back/src/chat/chat.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+
+
+@Injectable()
+export class ChatService {
+	constructor(private prisma: PrismaService) { }
+
+	async getChats(userId: number) {
+		return {};
+	}
+}
