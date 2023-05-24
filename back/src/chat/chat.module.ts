fd6596de3ce8db478b06fc96b94eaaaabb65bf56
karisti- <kepaaristi@gmail.com>
2023-05-24 20:02:38 +0200

    chat empty template

--- back/src/chat/chat.module.ts
+++ back/src/chat/chat.module.ts
@@ -2,15 +2,11 @@ import { Module } from '@nestjs/common';
 import { ChatController } from './chat.controller';
 import { ChatService } from './chat.service';
 import { ChatChannelModule } from './chat-channel/chat-channel.module';
-import { ChatChannelUserModule } from './chat-channel-user/chat-channel-user.module';
-import { ChatGateway } from './chat.gateway';
-import { UserService } from "../user/user.service";
-import { WebSocketService } from '../auth/websocket/websocket.service';
 
 @Module({
   controllers: [ChatController],
-  providers: [ChatService, ChatGateway, UserService, WebSocketService],
-  imports: [ChatChannelModule, ChatChannelUserModule
+  providers: [ChatService],
+  imports: [ChatChannelModule,
   ],
 })
 export class ChatModule { }
