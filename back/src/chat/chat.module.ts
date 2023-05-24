232fed009761631fe18775faf62b88c0ad4e4de5
karisti- <kepaaristi@gmail.com>
2023-05-24 20:10:46 +0200

    chat websocket at port 8083

--- back/src/chat/chat.module.ts
+++ back/src/chat/chat.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { ChatController } from './chat.controller';
 import { ChatService } from './chat.service';
 import { ChatChannelModule } from './chat-channel/chat-channel.module';
+import { ChatGateway } from './chat.gateway';
+import { UserService } from "../user/user.service";
+import { WebSocketService } from '../auth/websocket/websocket.service';
 
 @Module({
   controllers: [ChatController],
-  providers: [ChatService],
+  providers: [ChatService, ChatGateway, UserService, WebSocketService],
   imports: [ChatChannelModule,
   ],
 })
