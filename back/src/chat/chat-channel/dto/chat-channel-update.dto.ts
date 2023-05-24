c395750481ba13974850c0a263cbc13e0beb89b0
karisti- <kepaaristi@gmail.com>
2023-05-24 23:08:05 +0200

    chat channels endpoints

---
+++ back/src/chat/chat-channel/dto/chat-channel-update.dto.ts
@@ -0,0 +1,22 @@
+import { IsString, IsOptional, IsNumber, MinLength, MaxLength } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger"
+import { IsChannelName } from '../../../utils/decorators/is-channel-name.decorator';
+
+export class ChatChannelUpdateDto {
+
+	@ApiProperty()
+	@IsNumber()
+	id: number
+
+	@ApiProperty()
+	@IsString()
+	@MinLength(3)
+	@MaxLength(15)
+	@IsChannelName()
+	name: string
+
+	@ApiProperty()
+	@IsString()
+	@IsOptional()
+	password?: string
+}
