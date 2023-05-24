b609726523992ce56d5eafd2dc490c7d6161a804
karisti- <kepaaristi@gmail.com>
2023-05-24 20:02:56 +0200

    chat-channel empty template

--- back/src/chat/chat-channel/dto/index.ts
+++ back/src/chat/chat-channel/dto/index.ts
@@ -1,4 +1,8 @@
 //Barrel Export
 
+<<<<<<< HEAD
 export * from './chat-channel-create.dto';
 export * from './chat-channel-update.dto';
+=======
+export * from './chat-channel.dto';
+>>>>>>> ddd11c7 (chat-channel empty template)
