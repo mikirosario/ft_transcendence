1d0c2ba47843d399c1a7ffaa256fc826f904b417
karisti- <kepaaristi@gmail.com>
2023-05-24 23:08:05 +0200

    chat channels endpoints

--- back/src/chat/chat-channel/dto/index.ts
+++ back/src/chat/chat-channel/dto/index.ts
@@ -1,8 +1,4 @@
 //Barrel Export
 
-<<<<<<< HEAD
 export * from './chat-channel-create.dto';
 export * from './chat-channel-update.dto';
-=======
-export * from './chat-channel.dto';
->>>>>>> ddd11c7 (chat-channel empty template)
