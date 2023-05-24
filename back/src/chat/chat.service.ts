191073febad151f609f45e0f8dd40c95612ad00a
karisti- <kepaaristi@gmail.com>
2023-05-24 20:02:38 +0200

    chat empty template

--- back/src/chat/chat.service.ts
+++ back/src/chat/chat.service.ts
@@ -7,10 +7,6 @@ export class ChatService {
 	constructor(private prisma: PrismaService) { }
 
 	async getChats(userId: number) {
-		return {
-			channels: [],
-			directs: [],
-			blocked_users: []
-		};
+		return {};
 	}
 }
