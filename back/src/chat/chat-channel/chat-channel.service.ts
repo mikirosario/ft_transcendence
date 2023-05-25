9a0128e4525c6a0ac6c1a471fa2e2ae60d7fd8df
karisti- <kepaaristi@gmail.com>
2023-05-25 19:08:30 +0200

    todo

--- back/src/chat/chat-channel/chat-channel.service.ts
+++ back/src/chat/chat-channel/chat-channel.service.ts
@@ -64,6 +64,10 @@ export class ChatChannelService {
 		TODO: Comprobar si el usuario que lo intenta es administrador en el canal (ChannelUsers)
 		*/
 
+		/*
+		TODO: Comprobar si el usuario que lo intenta es administrador en el canal (ChannelUsers)
+		*/
+
 		if (dto.password != null && dto.password.length > 0)
 		{
 			hash = await argon.hash(dto.password);
