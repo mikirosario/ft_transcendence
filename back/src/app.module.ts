aa10e9458ba623dc2049e080c664128af4209533
karisti- <kepaaristi@gmail.com>
2023-06-17 12:47:07 +0200

    bookmark deleted

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config'
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ServeImageModule } from './utils/serve-image/serve-image.module';
 import { OAuthModule } from './oauth/oauth.module';
@@ -11,7 +10,7 @@ import { PongModule } from './pong/pong.module';
 import { ChatModule } from './chat/chat.module';
 
 @Module({
-	imports: [AuthModule, UserModule, BookmarkModule, PrismaModule, ServeImageModule,
+	imports: [AuthModule, UserModule, PrismaModule, ServeImageModule,
 		FriendModule, PongModule, ChatModule,
 		ConfigModule.forRoot({ isGlobal: true }),
 		OAuthModule,
