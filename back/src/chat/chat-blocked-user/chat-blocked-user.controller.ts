1b7273e8940afb834335f6f1cb114121e2137fe2
karisti- <kepaaristi@gmail.com>
2023-06-17 14:57:48 +0200

    chat blocked user

---
+++ back/src/chat/chat-blocked-user/chat-blocked-user.controller.ts
@@ -0,0 +1,27 @@
+import { Controller, UseGuards, Body, Post, Delete } from "@nestjs/common";
+import { ApiBody, ApiBearerAuth } from "@nestjs/swagger"
+import { JwtGuard } from "../../auth/guard";
+import { GetJwt } from "../../auth/decorator";
+import { ChatBlockedUserService } from "./chat-blocked-user.service";
+import { ChatBlockedDto } from './dto'
+
+
+@UseGuards(JwtGuard)
+@Controller('chat/blocks')
+@ApiBearerAuth()
+export class ChatBlockedUserController {
+	
+	constructor(private chatBlockedUserService: ChatBlockedUserService) { }
+
+	@Post()
+	@ApiBody({ type: ChatBlockedDto })
+	async chatBlockUser(@GetJwt('sub') userId: number, @Body() dto: ChatBlockedDto) {
+		return this.chatBlockedUserService.chatBlockUser(userId, dto);
+	}
+
+	@Delete()
+	@ApiBody({ type: ChatBlockedDto })
+	async chatUnblockUser(@GetJwt('sub') userId: number, @Body() dto: ChatBlockedDto) {
+		return this.chatBlockedUserService.chatUnblockUser(userId, dto);
+	}
+}
