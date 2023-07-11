a51dca77a6f072a92668c168361d39c0ee916456
karisti- <kepaaristi@gmail.com>
2023-07-11 22:51:53 +0200

    real time user status update

--- back/src/user/user-status.gateway.ts
+++ back/src/user/user-status.gateway.ts
@@ -21,65 +21,63 @@ export class UserGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
 {
 	@WebSocketServer() server: Server;
 	
-	constructor(private config: ConfigService, private userService: UserService, private webSocketService: WebSocketService) { }
+	constructor(private config: ConfigService, private userService: UserService,
+				private webSocketService: WebSocketService) { }
 
 	afterInit(server: any) { }
 	
 	async handleConnection(client: any, ...args: any[]) {
 		const userId = this.webSocketService.getUserIdFromHeaders(client.handshake.headers);
+
 		if (userId == null)
 		{
 			client.disconnect();
 			return;
 		}
 
-		const user = await this.userService.setUserStatus(userId, true);
-		if (user == null)
-		{
-			client.disconnect();
-			return;
-		}
+		try {
+			const user = await this.userService.setUserStatus(userId, true);
 
-		console.log('Hola! ' + user.nick + ' está online ✅');
+			if (user == null)
+			{
+				client.disconnect();
+				return;
+			}
+
+			this.sendSocketMessageToAll('UPDATE_USER', user);
+
+			console.log('Hola! ' + user.nick + ' está online ✅');
+		} catch (error) {
+		}
 	}
 	
 	async handleDisconnect(client: any) {
 		const userId = this.webSocketService.getUserIdFromHeaders(client.handshake.headers);
 		if (userId == null)
 		{
+			console.log('Alguien se ha ido del sitio ❌');
 			client.disconnect();
 			return;
 		}
 
-		const user = await this.userService.setUserStatus(userId, false);
-		if (user == null)
-		{
+		try {
+			const user = await this.userService.setUserStatus(userId, false);
+
+			if (user == null)
+			{
+				client.disconnect();
+				return;
+			}
+
+			this.sendSocketMessageToAll('UPDATE_USER', user);
+
+			console.log(user.nick + ' está offline ❌');
+		} catch (error) {
 			client.disconnect();
-			return;
 		}
-		
-		console.log(user.nick + ' está offline ❌');
 	}
 
-	/*
-	@SubscribeMessage('event_join')
-	handleJoinRoom(client: Socket, room: string) {
-		client.join(`room_${room}`);
+	async sendSocketMessageToAll(eventName: string, data: any) {
+		this.server.emit(eventName, data);
 	}
-	
-	@SubscribeMessage('event_message') //TODO Backend
-	handleIncommingMessage(client: Socket, payload: { room: string; message: string }) {
-		const { room, message } = payload;
-
-		console.log(payload)
-
-		this.server.to(`room_${room}`).emit('new_message', message);
-	}
-	
-	@SubscribeMessage('event_leave')
-	handleRoomLeave(client: Socket, room:string) {
-		console.log(`chao room_${room}`)
-		client.leave(`room_${room}`);
-	}
-	*/
 }
