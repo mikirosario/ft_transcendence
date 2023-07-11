03c42761f73badd26090a32e3b8dad387ec89307
gpernas- <gonper88@gmail.com>
2023-07-11 15:59:32 +0200

    URL Restrictions

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,20 +1,70 @@
-import React from 'react';
-import { Route, Link, Router } from "react-router-dom";
-import logo from './assets/images/logo.svg';
+import React, { useState, ReactElement } from 'react';
+import './assets/css/index.css';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from './pages/Homepage';
 import Options from './pages/Options';
-import './assets/css/App.css';
+import GameSelector from './pages/GameSelector';
+import PongPage from './pages/Pong';
+import Register from './pages/Register';
+import PreRegister from './pages/PreRegister';
+import Verification2af from './pages/Verification2AF';
+import { io, Socket } from 'socket.io-client';
+import { getServerIP } from './utils/utils';
+import NoPermissionPage from './pages/PermissionDenied';
 
-
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      
-
-
-    </div>
-  );
+const socketOptions = {
+  transportOptions: {
+    polling: {
+      extraHeaders: {
+        Authorization: 'Bearer ' + localStorage.getItem("token"),
+      }
+    }
+  }
 };
 
+// Puede que no sea necesario
+const socket: Socket = io(getServerIP(8081), socketOptions);
+
+const useAuth = () => {
+  const token = localStorage.getItem('token');
+  return !!token;
+};
+
+function ProtectedComponent({ children }: { children: React.ReactElement }) {
+  const isAuthenticated = useAuth();
+  return isAuthenticated ? children : <NoPermissionPage />;
+}
+
+function GuestComponent({ children }: { children: React.ReactElement }) {
+  const isAuthenticated = useAuth();
+  return !isAuthenticated ? children : <Navigate to="/homepage" replace />;
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div style={{
+        backgroundColor: '#0E1625',
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100vh',
+      }}></div>
+      <Routes>
+        <Route path="/" element={<GuestComponent><PreRegister /></GuestComponent>} />
+
+        {/* Falta alguna comprobacion para ver si le ha dado el boton para el login, si va por URL habria que bloquear */}
+        <Route path="/register" element={<GuestComponent><Register /></GuestComponent>} /> 
+
+        <Route path="/homepage" element={<ProtectedComponent><Home /></ProtectedComponent>} />
+        <Route path="/settings" element={<ProtectedComponent><Options /></ProtectedComponent>} />
+        <Route path="/pong" element={<ProtectedComponent><PongPage /></ProtectedComponent>} />
+        <Route path="/gameSelector" element={<ProtectedComponent><GameSelector /></ProtectedComponent>} />
+        <Route path="/verification" element={<ProtectedComponent><Verification2af /></ProtectedComponent>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default App;
