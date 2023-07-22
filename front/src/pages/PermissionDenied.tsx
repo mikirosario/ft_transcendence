53c412a0d3a248a8f5cf54d62d778a4756fff860
karisti- <kepaaristi@gmail.com>
2023-07-22 20:37:57 +0200

    minor

--- front/src/pages/PermissionDenied.tsx
+++ front/src/pages/PermissionDenied.tsx
@@ -1,7 +1,18 @@
 function NoPermissionPage() {
+    
+  const MsgStyle: React.CSSProperties = {
+    position: 'absolute',
+    top: '12%',
+    left: '48%',
+    fontSize: '28px',
+    fontFamily: "'Press Start 2P'",
+    color: 'grey',
+    transform: 'translateX(-50%)',
+    width: '50%'
+}
     return (
       <div>
-        <h1>No tienes permiso para acceder a esta página.</h1>
+        <h1 style={MsgStyle}>No tienes permiso para acceder a esta página.</h1>
       </div>
     );
   }
