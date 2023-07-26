305e2fff32ec7c5813905575ade42677a4c43612
karisti- <kepaaristi@gmail.com>
2023-07-26 22:37:12 +0200

    pong matches list done
    Co-authored-by: Gonzalo <Tooaad@users.noreply.github.com>

--- srcs/back/src/pong/pong-game-match/pong-game-match.service.ts
+++ srcs/back/src/pong/pong-game-match/pong-game-match.service.ts
@@ -13,50 +13,98 @@ export class PongGameMatchService {
 
 	async getProfileMatches(userId: number, nick: string) {
 
-		const user = await this.userService.getUserByNick(nick);
+		const user = await this.userService.getUserByNick(nick, {
+			id: true,
+			nick: true,
+			avatarUri: true,
+			gamesWon: true,
+			gamesLost: true,
+			gamesPlayed: true
+		});
 
-		const data = await this.prisma.gameMatch.findMany({
+		const gameMatches = await this.prisma.gameMatch.findMany({
 			where: {
-			  OR: [
-				{ userId1: 2 },
-				{ userId2: 2 },
-			  ],
+				OR: [
+						{ userId1: user.id },
+						{ userId2: user.id },
+				],
 			},
-		  });
+			include: {
+				user1: {
+					select: {
+						id: true,
+						nick: true,
+						avatarUri: true,
+					}
+				},
+				user2: {
+					select: {
+						id: true,
+						nick: true,
+						avatarUri: true,
+					}
+				},
+				winnerUser: {
+					select: {
+						id: true,
+					}
+				},
+			},
+		});
 
+		const rank = await this.getUserRank(user.nick);
 
-
-		return {
+		let result = {
 			user: {
-				userId: 1,
-				nick: 'karisti-',
-				avatarUri: 'basdlsad',
-				rank: 1,
-				wins: 42,
-				losses: 0,
+				userId: user.id,
+				nick: user.nick,
+				avatarUri: user.avatarUri,
+				rank: rank,
+				wins: user.gamesWon,
+				losses: user.gamesLost,
 			},
-			matches: [
-				{
-					user1: {
-						userId: 1,
-						nick: 'karisti-',
-						avatarUri: 'basdlsad',
-						score: 3,
-						isWinner: true,
-						rank: 1,
-					},
-					user2: {
-						userId: 2,
-						nick: 'gpernas-',
-						avatarUri: 'basdlsad',
-						score: 1,
-						isWinner: false,
-						rank: 2
-					},
-					matchEnded: true
-				}
-			]
+			matches: this.formatGameMatches(gameMatches)
 		};
+
+		return result;
 	}
+
+	private formatGameMatches(gameMatchesList: any) {
+		const gameMatchesListFormatted: any[] = gameMatchesList.map((match) => ({
+			user1: {
+				userId: match.user1.id,
+				nick: match.user1.nick,
+				avatarUri: match.user1.avatarUri,
+				score: match.score1,
+				isWinner: match.user1.id == match.winnerUser.id,
+			},
+			user2: {
+				userId: match.user2.id,
+				nick: match.user2.nick,
+				avatarUri: match.user2.avatarUri,
+				score: match.score2,
+				isWinner: match.user2.id == match.winnerUser.id,
+			},
+			matchEnded: match.hasEnded
+		}));
+
+		return gameMatchesListFormatted;
+	}
+
+	private async getUserRank(nick: string) {
+		const users = await this.prisma.user.findMany();
+		
+		// Calcular el ratio para cada usuario
+		const usersWithRatio = users.map(user => ({
+			...user,
+			ratio: user.gamesWon / user.gamesLost,
+		}));
+	
+		const sortedUsers = usersWithRatio.sort((a, b) => b.ratio - a.ratio);
+
+		const rank = sortedUsers.findIndex(user => user.nick === nick) + 1;
+	
+		return rank;
+	  }
 
 }
