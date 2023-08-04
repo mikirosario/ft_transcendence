42c021623854b6fbc9fddd2a98ac491660fb1c62
gpernas- <gonper88@gmail.com>
2023-08-04 09:58:30 +0200

    RemoveVerification while leaving

--- srcs/front/src/components/B_Logout.tsx
+++ srcs/front/src/components/B_Logout.tsx
@@ -1,18 +1,20 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FaSignOutAlt } from 'react-icons/fa';
 import { SocketContext1, SocketContext2 } from '../SocketContext'
+import { get2FAuthUserVerificationRemove } from '../requests/User.Service';
 
 const LogoutButton: React.FC = () => {
     const socket1 = useContext(SocketContext1);
     const socket2 = useContext(SocketContext2);
 
-    const handleLogout = () => {
+    const handleLogout = async () => {
         socket1?.off();
         socket2?.off();
         socket1?.disconnect();
         socket2?.disconnect();
-        localStorage.removeItem('token')
+        await get2FAuthUserVerificationRemove();
+        localStorage.removeItem('token');
     };
 
     const [buttonStyle, setButtonStyle] = useState<React.CSSProperties>({
@@ -41,11 +43,11 @@ const LogoutButton: React.FC = () => {
     };
 
     return (
-        <Link to={'/'} style={{ textDecoration: 'none' }}> 
-            <button style={buttonStyle} 
-                    onClick={handleLogout} 
-                    onMouseOver={() => setButtonStyle(LogoutButtonHoverStyle)} 
-                    onMouseOut={() => setButtonStyle({...buttonStyle, transform: 'scale(1)',})}>
+        <Link to={'/'} style={{ textDecoration: 'none' }}>
+            <button style={buttonStyle}
+                onClick={handleLogout}
+                onMouseOver={() => setButtonStyle(LogoutButtonHoverStyle)}
+                onMouseOut={() => setButtonStyle({ ...buttonStyle, transform: 'scale(1)', })}>
                 <FaSignOutAlt style={LogoutIconStyle} />
                 Desconectar
             </button>
