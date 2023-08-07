03f87ebcb20ae440862b90c1abc36953cbc1be77
gpernas- <gonper88@gmail.com>
2023-08-08 01:14:19 +0200

    Retar Notification to other players in back

--- srcs/back/src/pong/pong-game-match/dto/pong-duel.dto.ts
+++ srcs/back/src/pong/pong-game-match/dto/pong-duel.dto.ts
@@ -1,8 +1,12 @@
-import { IsNumber } from "class-validator";
+import { IsNumber, IsBoolean } from "class-validator";
 import { ApiProperty } from "@nestjs/swagger";
 
 export class PongDuelDto {
 	@ApiProperty()
 	@IsNumber()
 	otherUserId: number
+
+	@ApiProperty()
+	@IsBoolean()
+	isOriginal: boolean
 }
